c6b532e24f699c85bcd22749ff5d8a1e5577b52b
bunchmj <matt.bunch@defenseunicorns.com>
2023-10-23 15:22:30 -0500

    wip

---
+++ examples/complete/variables.tf
@@ -0,0 +1,20 @@
+variable "name_prefix" {
+  description = "The prefix to use when naming all resources"
+  type        = string
+  validation {
+    condition     = length(var.name_prefix) <= 20
+    error_message = "The name prefix cannot be more than 20 characters"
+  }
+  default = "ci"
+}
+
+variable "region" {
+  description = "The AWS region to deploy into"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
