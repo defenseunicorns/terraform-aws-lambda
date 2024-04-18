c52b2fea738c979ba7e0fff9fba5c21a31188ba7
Zack A <24322023+zack-is-cool@users.noreply.github.com>
2024-04-17 20:59:34 -0700

    feat: reliable native Secrets Manager secret rotation using Lambda (#30)

--- examples/complete/providers.tf
+++ examples/complete/providers.tf
@@ -9,6 +9,22 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 3.1.0"
     }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.1.0"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = ">= 2.0.0"
+    }
   }
 }
 
